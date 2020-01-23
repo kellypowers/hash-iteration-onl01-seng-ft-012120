7ab671164fdf46a02887db93e0ea5147f2bbfb66
Kelly Powers <kpowers.nj@gmail.com>
2020-01-23 00:04:39 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -19,6 +19,6 @@ end
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+    return "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
