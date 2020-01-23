6e046c906ba54392b4b67d86f6406a79f32ec33f
Kelly Powers <kpowers.nj@gmail.com>
2020-01-23 00:07:37 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -17,8 +17,8 @@ def select_winner(passengers)
   winner
 end
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
-  end
-end
+# def happy_birthday(birthday_kids)
+#   birthday_kids.each do |name, age|
+#     puts "Happy Birthday #{name}! You are now #{age} years old!"
+#   end
+# end
