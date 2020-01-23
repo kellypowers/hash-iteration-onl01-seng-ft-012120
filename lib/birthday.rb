eda105029208be71b0df97f1dd8faaf4493961fb
Kelly Powers <kpowers.nj@gmail.com>
2020-01-23 00:09:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,9 +3,8 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
-
-
